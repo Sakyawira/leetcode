79fadaaa065ff2173ea4dfc7e504cf0148574f71
Sakyawira <sakyawira@gmail.com>
2021-04-21 23:35:33 +1200

    buy stock question is finished

--- leetcode/BuySellStock/Source.cpp
+++ leetcode/BuySellStock/Source.cpp
@@ -2,21 +2,24 @@
 #include <optional>
 #include <iostream>
 #include <utility>
+#include <algorithm>
+
 
 class Solution {
 public:
     int maxProfit(std::vector<int>& prices) {
         std::optional<int> buyPrice;
+        std::optional<int> currentProfit;
+
+        int index = 0;
         for (int price : prices) {
-            if (buyPrice.has_value()) {
-                if (price < buyPrice.value()) {
-                    buyPrice = price;
-                }
-                continue;
-            }
-            buyPrice = price;
+            // minimum price from day 0 to current day
+            buyPrice = std::min(buyPrice.value_or(INT_MAX), price);
+
+            // get the largest profit between last profit and the new one (difference between current value and current buy value) 
+            currentProfit = std::max(currentProfit.value_or(0),  price - buyPrice.value_or(INT_MAX));
         }
-        return buyPrice.value_or(0);
+        return currentProfit.value_or(0);
     }
 };
 
